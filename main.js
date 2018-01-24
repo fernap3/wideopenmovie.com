eae0fd582f502d03cb4e008dfbaeaaa01826888a
Peter Fernandes <supersonicandtails@gmail.com>
2018-01-24 14:01:49 -0500

    Floating headers complete

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var incomingHeader;
+var outgoingHeader;
+
 function onPageLoad()
 {
 	const navbar = document.getElementById("navbar");
@@ -14,35 +17,75 @@ function onPageLoad()
 	}
 
 	const aboutHeader = document.querySelector("#pane-about > h1");
-	const incomingHeader = aboutHeader.cloneNode(true);
+	incomingHeader = aboutHeader.cloneNode(true);
 	incomingHeader.classList.add("floating-section-header");
+	outgoingHeader = aboutHeader.cloneNode(true);
+	outgoingHeader.classList.add("floating-section-header");
 	document.body.appendChild(incomingHeader);
+	document.body.appendChild(outgoingHeader);
 
 	window.onscroll = () =>
 	{
-		incomingHeader.textContent = getFloatingSectionHeaderText();
-		//getFloatingSectionHeaderPosition();
-
-		const currentPane = getCurrentPane();
-
-		if (currentPane && currentPane.id !== "pane-home")
-		{
-			const paneHeader = currentPane.querySelector(".pane-title");
-			const paneTitle = paneHeader.textContent;
-			const paneTitleRect = paneHeader.getBoundingClientRect();
-			incomingHeader.style.left = paneTitleRect.left + "px";
-			incomingHeader.style.top = Math.max(navbar.offsetHeight / 2 - paneHeader.offsetHeight / 2, paneTitleRect.top) + "px";
-			incomingHeader.textContent = paneTitle;
-			incomingHeader.style.display = "";
-		}
-		else
-		{
-			incomingHeader.style.display = "none";
-		}
-
-
-		
+		renderIncomingHeader();
+		renderOutgoingHeader();
 	};
+}
+
+function renderOutgoingHeader()
+{
+	const prevPane = getPreviousPane();
+	const curPane = getCurrentPane();
+	if (prevPane && prevPane.id !== "pane-home")
+	{
+		const prevPaneHeader = prevPane.querySelector(".pane-title");
+		const paneTitle = prevPaneHeader.textContent;
+		const paneTitleRect = prevPaneHeader.getBoundingClientRect();
+		outgoingHeader.style.left = paneTitleRect.left + "px";
+		outgoingHeader.style.top = navbar.offsetHeight / 2 - prevPaneHeader.offsetHeight / 2 + "px";
+		outgoingHeader.textContent = paneTitle;
+
+		const incomingHeaderRect = incomingHeader.getBoundingClientRect();
+		const outgoingHeaderRect = outgoingHeader.getBoundingClientRect();
+
+		outgoingHeader.style.opacity = Math.min(1, (incomingHeaderRect.top - outgoingHeaderRect.top) / 100) + "";
+
+		outgoingHeader.style.display = "";
+	}
+	else
+	{
+		outgoingHeader.style.display = "none";
+	}
+}
+
+function renderIncomingHeader()
+{
+	const currentPane = getCurrentPane();
+	if (currentPane && currentPane.id !== "pane-home")
+	{
+		const paneHeader = currentPane.querySelector(".pane-title");
+		const paneTitle = paneHeader.textContent;
+		const paneTitleRect = paneHeader.getBoundingClientRect();
+		incomingHeader.style.left = paneTitleRect.left + "px";
+		incomingHeader.style.top = Math.max(navbar.offsetHeight / 2 - paneHeader.offsetHeight / 2, paneTitleRect.top) + "px";
+		incomingHeader.textContent = paneTitle;
+		incomingHeader.style.display = "";
+	}
+	else
+	{
+		incomingHeader.style.display = "none";
+	}
+}
+
+function getPreviousPane()
+{
+	const panes = Array.from(document.querySelectorAll(".pane"));
+	const currentPane = getCurrentPane();
+
+	for (let i = 0; i < panes.length - 1; i++)
+	{
+		if (panes[i + 1] === currentPane)
+			return panes[i];
+	}
 }
 
 function getCurrentPane()
@@ -61,14 +104,4 @@ function isScrolledPastPane(pane)
 	const navbar = document.getElementById("navbar");
 	const paneBounds = pane.getBoundingClientRect();
 	return paneBounds.top <= navbar.offsetHeight;
-}
-
-function getIncomingHeaderTop()
-{
-
-}
-
-function getFloatingSectionHeaderText()
-{
-	return "About";
 }
