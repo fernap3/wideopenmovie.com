813f7bd4109f71f2ae3f68b20060642ac3816094
Peter Fernandes <supersonicandtails@gmail.com>
2018-01-27 18:05:20 -0500

    Minify javascript output

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require("gulp");
 const ts = require("gulp-typescript");
 const sourcemaps = require("gulp-sourcemaps");
 const rimraf = require("rimraf");
+const uglify = require("gulp-uglifyes");
+const rename = require("gulp-rename");
 
 gulp.task("build-debug", () =>
 {
@@ -21,14 +23,16 @@ gulp.task("build-debug", () =>
 
 gulp.task("build-deploy", () =>
 {
-	gulp.src(["images/**/*"]).pipe(gulp.dest("deploy/images"));
-	gulp.src(["src/**/*.html"]).pipe(gulp.dest("deploy"));
-	gulp.src(["src/**/*.css"]).pipe(gulp.dest("deploy"));
-
 	rimraf("deploy", () => {
+		gulp.src(["images/**/*"]).pipe(gulp.dest("deploy/images"));
+		gulp.src(["src/**/*.html"]).pipe(gulp.dest("deploy"));
+		gulp.src(["src/**/*.css"]).pipe(gulp.dest("deploy"));
+
 		const tsProject = ts.createProject("tsconfig.json");
 		return tsProject.src()
 			.pipe(tsProject()).js
+			.pipe(gulp.dest("deploy/"))
+			.pipe(uglify())
 			.pipe(gulp.dest("deploy/"));
 	});
 });
